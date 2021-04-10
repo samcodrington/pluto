3e216cd1e66767fb232e22b3293787a6fb4360bf
Sam Codrington <sam.codrington@queensu.ca>
2021-04-10 12:37:25 -0400

    fix local-build script

--- launch-local-stack.sh
+++ launch-local-stack.sh
@@ -1,1 +1,24 @@
-docker-compose -f ./docker-compose.yml up
+#!/bin/bash
+BUILD=0
+while getopts "bh" option;
+do 
+  case $option in
+  b)
+    BUILD=1
+    echo "Building stack before running"
+    ;;
+  h)
+    echo `
+      -b        Run stack with build options
+      -h        Display this message
+    `
+  esac
+done
+if [ $BUILD -eq 1 ] 
+then
+  echo "Build started"
+  docker-compose build
+  echo "Build complete"
+fi
+
+docker-compose up
